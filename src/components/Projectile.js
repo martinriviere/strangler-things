434ad4ddbd40bb4d465a32c5b38c303ec87c698d
Martin Rivière <martin.riviere@gmail.com>
2019-10-23 17:27:15 +0200

    trajectoire aléatoire sans collision

--- src/components/Projectile.js
+++ src/components/Projectile.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { randomOf } from "./helpers";
 
 class Projectile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      y: "16.2%",
+      y: 0,
+      coef: Math.random() * 1.5 * (Math.random() < 0.5 ? -1 : 1),
       active: true
     };
     this.myRef = React.createRef();
@@ -14,8 +16,8 @@ class Projectile extends Component {
   fallsDown = () => {
     const { y } = this.state;
     // console.log(y);
-    if (y < "62%") {
-      this.setState({ y: `${parseFloat(y.slice(0, y.length - 1)) + 0.5}%` });
+    if (y < 62) {
+      this.setState({ y: y + 0.5 });
     } else {
       this.setState({ active: false });
     }
@@ -33,11 +35,9 @@ class Projectile extends Component {
   }
 
   render() {
-    const { y, display } = this.state;
+    const { y, coef } = this.state;
     const { projectile } = this.props;
-    const size =
-      40 *
-      (1 + (0.89 * (parseInt(y.slice(0, y.length - 1)) - 16.2)) / (62 - 16.2));
+    const size = 40 * (1 + (0.89 * (y - 16.2)) / (62 - 16.2));
     return (
       <img
         ref={this.myRef}
@@ -46,8 +46,8 @@ class Projectile extends Component {
           ...styles.projectile,
           width: size,
           marginLeft: -size / 2,
-          top: y,
-          display: display
+          top: y + "%",
+          left: 50 + y * coef + "%"
         }}
         alt=""
       />
@@ -62,7 +62,6 @@ const styles = {
     // backgroundImage: Doughnut,
     // backgroundSize: "cover",
     position: "absolute",
-    left: "50%",
     filter: "drop-shadow(0 30px 2px rgba(0, 0, 0, 0.3))"
   }
 };
--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,1 +1,2 @@
 export const randomOf = unit => Math.floor(Math.random() * unit);
+export const getValueFromPercentage = number => parseFloat(number.slice(0, number.length - 1))
