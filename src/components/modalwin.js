a3335dfcd2a13da1e163e36aced0aa0deb0ff4ad
Signupz <ludovic.mazet@gmail.com>
2019-10-24 10:30:39 +0200

    push avant pull de dev

---
+++ src/components/modalwin.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class ModalWin extends Component {
+    constructor() {
+        super()
+        this.state = {
+            display : false
+        }
+    }
+    render()
+
+
+
+
+
+
+}
+
+export default ModalWin;
