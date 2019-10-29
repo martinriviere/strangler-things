153e8218c4bee9e43fd98686982eb4c6e8522285
Signupz <ludovic.mazet@gmail.com>
2019-10-29 16:47:40 +0100

    Modification des modals pour les rendres modifiables plus facilement, création d'un fichier css dédié aux modals

--- src/components/modalwin.js
+++ src/components/modalwin.js
@@ -1,44 +1,68 @@
-import React, {Component} from "react";
-import Modal from "react-responsive-modal";
-import "../index";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import "./modal.css";
+// import Modal from "react-responsive-modal";
 
 class ModalWin extends Component {
-  state = {
-    open: true
-  };
-
-  onOpenModal = () => {
-    this.setState({ open: true });
-  };
-
-  onCloseModal = () => {
-    this.setState({ open: false });
-  };
 
   render() {
-    const { open } = this.state;
     return (
-      <div className="modal-div">
-        <button onClick={this.onOpenModal}>Open modal</button>
-        <Modal
-          open={open}
-          onClose={this.onCloseModal}
-          center
-          styles={{ overlay: { background: "rgba(0, 0, 0, 0.47)" } }}
-        >
-          <div id="modalLose">
-            <p>YOU WIN ! !</p>
-            <img
-              src="http://giphygifs.s3.amazonaws.com/media/A6aHBCFqlE0Rq/giphy.gif"
-              alt="winner"
-            />
-            <p>Continue</p>
-            <p>Back to title</p>
-          </div>
-        </Modal>
+      <div id="ModalWin">
+        <p>YOU WIN !!</p>
+        <img 
+          src="http://giphygifs.s3.amazonaws.com/media/jUwpNzg9IcyrK/giphy.gif"
+          alt="winner"
+        />
+        <p>Continue</p>
+        <p>Back to title</p>
+        {/* <Link to="/level/2"><p>Continue</p></Link>
+        <Route path="/level/2" exact component={LevelTwo} />
+        <Link to="/"><p>Back to title</p></Link>
+        <Route path="/" exact component={Home} /> */}
       </div>
     );
   }
 }
 
 export default ModalWin;
+
+// class ModalWin extends Component {
+//   state = {
+//     open: true
+//   };
+
+//   onOpenModal = () => {
+//     this.setState({ open: true });
+//   };
+
+//   onCloseModal = () => {
+//     this.setState({ open: false });
+//   };
+
+//   render() {
+//     const { open } = this.state;
+//     return (
+//       <div className="modal-div">
+//         <button onClick={this.onOpenModal}>Open modal</button>
+//         <Modal
+//           open={open}
+//           onClose={this.onCloseModal}
+//           center
+//           styles={{ overlay: { background: "rgba(0, 0, 0, 0.47)" } }}
+//         >
+//           <div id="modalLose">
+//           <p>YOU WIN ! !</p>
+//             <img
+//               src="http://giphygifs.s3.amazonaws.com/media/A6aHBCFqlE0Rq/giphy.gif"
+//               alt="winner"
+//             />
+//             <p>Continue</p>
+//             <p>Back to title</p>
+//           </div>
+//         </Modal>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ModalWin;
