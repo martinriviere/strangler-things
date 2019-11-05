52f8d1b76a6094205a7b4408faf5cce167923699
Signupz <ludovic.mazet@gmail.com>
2019-11-05 13:08:14 +0100

    Modification taille police modal, modification des gif de modal victoire/defaite, mise en forme du menu d'acceuil

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TitleScreen from "../Design/Images/TitleScreenMenu.jpg"
-import './Menu.css';
 import { Link } from "react-router-dom";
+import './Menu.css';
 
 let divStyle = {
     backgroundImage: `url(${TitleScreen})`,
--- src/components/modallose.js
+++ src/components/modallose.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./modal.css";
 import { Link } from "react-router-dom";
-// import Modal from "react-responsive-modal";
 
 class ModalLose extends Component {
   render() {
@@ -10,14 +9,14 @@ class ModalLose extends Component {
         <div id="ModalLose">
           <p>YOU LOSE !!</p>
           <img
-            src="http://giphygifs.s3.amazonaws.com/media/A6aHBCFqlE0Rq/giphy.gif"
+            src="http://giphygifs.s3.amazonaws.com/media/jUwpNzg9IcyrK/giphy.gif"
             alt="winner"
           />
           <p>
-            <Link to="/game">Continue</Link>
+            <Link to="/game">Try again</Link>
           </p>
           <p>
-            <Link to="/">Back to title</Link>
+            <Link to="/">Back to menu</Link>
           </p>
         </div>
       </div>
@@ -26,44 +25,3 @@ class ModalLose extends Component {
 }
 
 export default ModalLose;
-
-// class ModalLose extends Component {
-//   state = {
-//     open: false
-//   };
-
-//   onOpenModal = () => {
-//     this.setState({ open: true });
-//   };
-
-//   onCloseModal = () => {
-//     this.setState({ open: false });
-//   };
-
-//   render() {
-//     const { open } = this.state;
-//     return (
-//       <div className="modal-div">
-//         <button onClick={this.onOpenModal}>Open modal</button>
-//         <Modal
-//           open={open}
-//           onClose={this.onCloseModal}
-//           center
-//           styles={{ overlay: { background: "rgba(0, 0, 0, 0.47)" } }}
-//         >
-//           <div id="modalLose">
-//           <p>YOU LOSE ! !</p>
-//               <img
-//                 src="https://media.giphy.com/media/a93jwI0wkWTQs/giphy.gif"
-//                 alt="loser"
-//               />
-//               <p>Continue</p>
-//               <p>Back to title</p>
-//           </div>
-//         </Modal>
-//       </div>
-//     );
-//   }
-// }
-
-// export default ModalLose;
