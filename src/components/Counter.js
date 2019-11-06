f322281829c46a5751c71cc3f887eed4777c7d91
Axel Bizel <axel.bizel@gmail.com>
2019-11-06 16:03:55 +0100

    rien de spécial rien de special

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,38 +1,43 @@
 import React, { Component } from "react";
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
+import "./counter.css";
 
-const counterStyle = {
-  fontSize: "30px",
-  fontFamily: "SimpsonFont",
-  color: "white",
-  textShadow: "0 0 10px #000000"}
-  ;
-
+// const counterStyle = {
+//   fontSize: "30px",
+//   fontFamily: "SimpsonFont",
+//   color: "white",
+//   textShadow: "0 0 10px #000000"
+// };
 class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
-    this.prevCount=0;
+    this.state = {};
+    this.prevCount = 0;
   }
 
   componentDidUpdate() {
-    this.prevCount = this.props.count
+    this.prevCount = this.props.count;
   }
 
   render() {
     return (
       <div
         className="counter-container"
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          position: "absolute",
-          right: "5vw",
-          top: "20vh"
-        }}
+        // style={{
+        //   flex: 1,
+        //   flexDirection: "column",
+        //   position: "absolute",
+        //   right: "5vw",
+        //   top: "20vh"
+        // }}
       >
-        <CountUp style={counterStyle} start={this.prevCount} end={this.props.count} />
+        <CountUp
+          // style={counterStyle}
+          className="counter-display"
+          start={this.prevCount}
+          end={this.props.count}
+        />
+        <p>Score</p>
       </div>
     );
   }
