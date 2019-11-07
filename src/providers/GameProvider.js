245a83a1639b4ecdc34d10a2604f907a8161aa50
Martin Rivière <martin.riviere@gmail.com>
2019-11-07 10:23:55 +0100

    sauvegarde du compteur quand on quitte le jeu

--- src/providers/GameProvider.js
+++ src/providers/GameProvider.js
@@ -6,7 +6,7 @@ class GameProvider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      level: localStorage.getItem("level") || 1,
+      level: parseInt(localStorage.getItem("level")) || 1,
       nextLevel: this.nextLevel,
       resetLevel: this.resetLevel,
       nbProjectiles: 5
@@ -24,14 +24,15 @@ class GameProvider extends Component {
   resetLevel = () => {
     this.setState({ level: 1 });
     localStorage.setItem("level", 1);
+    localStorage.setItem("count", 0);
   };
 
   nextLevel = () => {
     this.setState({
-      level: parseInt(this.state.level) + 1,
-      nbProjectiles: 5 * (parseInt(this.state.level) + 1)
+      level: this.state.level + 1,
+      nbProjectiles: 5 * (this.state.level + 1)
     });
-    localStorage.setItem("level", parseInt(this.state.level) + 1);
+    localStorage.setItem("level", this.state.level + 1);
   };
 
   render() {
