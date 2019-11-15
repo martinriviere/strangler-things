94947432d7c826aaefb16e0cba3de0e3542fd3da
Axel Bizel <axel.bizel@gmail.com>
2019-11-15 11:11:54 +0100

    ajout gif Modal Drunk

--- src/components/ModalDrunk.js
+++ src/components/ModalDrunk.js
@@ -1,18 +1,28 @@
 import React, { useContext } from "react";
 import { Translate } from "react-localize-redux";
 import Drunk from "../Design/Sounds/homer-drunk.mp3";
+import HomerDrunk from "../Design/Images/homer-drunk.gif";
 import { GameContext } from "../providers/GameProvider";
 import "../App.css";
 
 function ModalDrunk(props) {
-    const { isFxOn } = useContext(GameContext);
-    return (
-      <div className="containerDrunk">
-        <div className="ModalDrunk">
-          <h2 className="homerDrunk"><Translate id="modal.drunk-title" /></h2>
-          <p><Translate id="modal.drunk-text" /></p>
-        </div>
-        {isFxOn && (
+  const { isFxOn } = useContext(GameContext);
+  return (
+    <div className="containerDrunk">
+      <div className="ModalDrunk">
+        <img
+          src="https://media3.giphy.com/media/10OOPKuNawOB8c/giphy.gif"
+          alt="winner"
+        />
+        <h2 className="homerDrunk">
+          <Translate id="modal.drunk-title" />
+        </h2>
+
+        <p>
+          <Translate id="modal.drunk-text" />
+        </p>
+      </div>
+      {isFxOn && (
         <iframe
           title="son-drunk"
           src={Drunk}
@@ -21,8 +31,8 @@ function ModalDrunk(props) {
           style={{ visibility: "hidden" }}
         ></iframe>
       )}
-      </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default ModalDrunk;
